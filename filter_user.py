1264b83a3b8a90c2c651c245b6bc08428fa7bc6a
J-Miguel Rocha Ramos <j-miguelrocharamos@MacBook-Pro-2.local>
2026-04-22 14:02:01 +0200

    improve the main prompt

--- filter_user.py
+++ filter_user.py
@@ -36,7 +36,9 @@ def filter_users_by_name(name):
 
 if __name__ == "__main__":
     filter_option = input(
-        "What would you like to filter by? (Currently, only 'name' and 'age' is supported): ").strip().lower()
+        "What would you like to filter by? (Currently, only 'name', 'age' "
+        "and 'email'"
+        " is supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
